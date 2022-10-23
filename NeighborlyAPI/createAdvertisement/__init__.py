0d2222e3f4d323e2424c353a703ebcc53000eaca
CamNguyen260399 <camnguyen2603@gmail.com>
2022-10-23 16:08:50 +0700

    Neighborly App with Azure Functions

---
+++ NeighborlyAPI/createAdvertisement/__init__.py
@@ -0,0 +1,30 @@
+import azure.functions as func
+import pymongo
+import os
+import json
+from bson.json_util import dumps
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    request = req.get_json()
+
+    if request:
+        try:
+            url = os.environ["MyDbConnection"]  # TODO: Update with appropriate MongoDB connection information
+            client = pymongo.MongoClient(url)
+            database = client['neighborlyproject']
+            collection = database['neighborlyadvertisements']
+
+            rec_id1 = collection.insert_one(eval(request))
+
+            return func.HttpResponse(req.get_body())
+
+        except ValueError:
+            print("could not connect to mongodb")
+            return func.HttpResponse('Could not connect to mongodb', status_code=500)
+
+    else:
+        return func.HttpResponse(
+            "Please pass name in the body",
+            status_code=400
+        )
